b10dbab59d380a57264fc79ecc7f7d15b9f1c8d9
alan-404 <nguyentri.alan@gmail.com>
2024-06-23 13:36:53 +0700

    bpe setup

---
+++ train_bpe.py
@@ -0,0 +1,20 @@
+from tokenizers import Tokenizer
+from tokenizers.models import BPE
+from tokenizers.pre_tokenizers import Whitespace
+from tokenizers.trainers import BpeTrainer
+
+from typing import List
+
+import fire
+
+def train(text_paths: List[str],
+          saved_path: str):
+    tokenizer = Tokenizer(BPE())
+    trainer = BpeTrainer(special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
+    tokenizer.pre_tokenizer = Whitespace()
+    tokenizer.train(files=text_paths, trainer=trainer)
+
+    tokenizer.save(saved_path)
+
+if __name__ == '__main__':
+    fire.Fire(train)
